71c0e38d6fafad5c59febf073d8232549d988a7b
Matthew Fulmer <Tapple Gao>
2012-12-28 00:02:24 -0700

    do faster sensor sampling, and use a simple low pass filter to reduce some noise

--- src/org/tavatar/tavimator/Gyroscope.java
+++ src/org/tavatar/tavimator/Gyroscope.java
@@ -27,8 +27,8 @@ public class Gyroscope implements SensorEventListener {
 	}
  
 	protected void onResume() {
-        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
-        sensorManager.registerListener(this, magnetometer, SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_GAME);
+        sensorManager.registerListener(this, magnetometer, SensorManager.SENSOR_DELAY_GAME);
     }
 
     protected void onPause() {
@@ -37,14 +37,15 @@ public class Gyroscope implements SensorEventListener {
 
  
 	public void onSensorChanged(SensorEvent evt) {
+		final float smoothing = 1.0f / 4.0f;
 		if (evt.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
-			gravity[0] = evt.values[0];
-			gravity[1] = evt.values[1];
-			gravity[2] = evt.values[2];
+			gravity [0] = gravity [0] * (1.0f - smoothing) + evt.values[0] * smoothing;
+			gravity [1] = gravity [1] * (1.0f - smoothing) + evt.values[1] * smoothing;
+			gravity [2] = gravity [2] * (1.0f - smoothing) + evt.values[2] * smoothing;
 		} else if (evt.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
-			magnetic[0] = evt.values[0];
-			magnetic[1] = evt.values[1];
-			magnetic[2] = evt.values[2];
+			magnetic[0] = magnetic[0] * (1.0f - smoothing) + evt.values[0] * smoothing;
+			magnetic[1] = magnetic[1] * (1.0f - smoothing) + evt.values[1] * smoothing;
+			magnetic[2] = magnetic[2] * (1.0f - smoothing) + evt.values[2] * smoothing;
 		}
 
 	}
