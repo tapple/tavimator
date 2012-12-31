b7576feee5ce6f83cb96b21e777d734741a002d3
Matthew Fulmer <Tapple Gao>
2012-12-31 09:26:06 -0700

    converted rotation.cpp and rotation.h from qavimator to java

---
+++ src/org/tavatar/tavimator/Position.java
@@ -0,0 +1,23 @@
+package org.tavatar.tavimator;
+
+public class Position {
+	public float x;
+	public float y;
+	public float z;
+	
+	public Position() {
+	  x=0;
+	  y=0;
+	  z=0;
+	}
+
+	public Position(float px, float py, float pz) {
+	  x=px;
+	  y=py;
+	  z=pz;
+	}
+
+	public Position difference(Position pos1, Position pos2) {
+	  return new Position(pos2.x-pos1.x,pos2.y-pos1.y,pos2.z-pos1.z);
+	}
+}
---
+++ src/org/tavatar/tavimator/Rotation.java
@@ -0,0 +1,23 @@
+package org.tavatar.tavimator;
+
+public class Rotation {
+	public float x;
+	public float y;
+	public float z;
+	
+	public Rotation() {
+	  x=0;
+	  y=0;
+	  z=0;
+	}
+
+	public Rotation(float rx, float ry, float rz) {
+	  x=rx;
+	  y=ry;
+	  z=rz;
+	}
+
+	public Rotation difference(Rotation rot1, Rotation rot2) {
+	  return new Rotation(rot2.x-rot1.x,rot2.y-rot1.y,rot2.z-rot1.z);
+	}
+}
