71a13d0f9cf2d2efadd0468b0b9b60bcdfae0aeb
Matthew Fulmer <Tapple Gao>
2013-01-10 11:20:26 -0700

    replaced the lighting model with the one used in qavimator, but with less ambient light

--- res/raw/color_vertex_shader.glsl
+++ res/raw/color_vertex_shader.glsl
@@ -3,27 +3,50 @@ uniform mat4 u_MVMatrix;	// A constant representing the combined model/view matr
 uniform vec3 u_LightPos;	// The position of the light in eye space.
 uniform vec4 u_Color;		// Object color information we will pass in.
 
-attribute vec4 a_Position;	// Per-vertex position information we will pass in.
-attribute vec3 a_Normal;	// Per-vertex normal information we will pass in.
+attribute vec4 a_Position;	// input. Vertex position in model coordinates
+attribute vec3 a_Normal;	// input. Vertex normal   in model coordinates
 
 varying vec4 v_Color;		// This will be passed into the fragment shader.
 
+vec3 vertexPosition;		// vertex position in eye coordinates
+vec3 vertexNormal;			// vertex normal   in eye coordinates
+
+const vec3 light0Position = vec3(0, 80, 100);
+const vec4 light0DiffuseColor = vec4(1.0, 1.0, 1.0, 1.0);
+
+const vec3 light1Position = vec3(0, 80, -100);
+const vec4 light1DiffuseColor = vec4(0.5, 0.5, 0.5, 1.0);
+
+const vec4  ambientColor = vec4(0.1, 0.1, 0.1, 1.0);
+const vec4 specularColor = vec4(0.6, 0.6, 0.6, 0.0);
+const float shininess = 100.0;
+
+float pdot(const vec3 v1, const vec3 v2) {
+	return max(dot(v1, v2), 0.0);
+}
+
+vec4 lightVertex(vec3 lightPosition, vec4 lightDiffuseColor) {
+	vec3 lightDirection = normalize(lightPosition - vertexPosition);
+	float diffuseFactor = pdot(vertexNormal, lightDirection);
+	float specularFactor = 0.0;
+	if (diffuseFactor != 0.0) {
+		specularFactor = pow(pdot(vertexNormal, normalize(lightDirection + vec3(0,0,1))), shininess);
+	}
+	// Multiply the color by the illumination level. It will be interpolated across the triangle.
+	return u_Color * lightDiffuseColor * diffuseFactor + specularColor * specularFactor;
+}
+
 void main() {				// The entry point for our vertex shader.
 	// Transform the vertex into eye space.
-	vec3 modelViewVertex = vec3(u_MVMatrix * a_Position);
+	vertexPosition = vec3(u_MVMatrix * a_Position);
 	// Transform the normal's orientation into eye space.
-	vec3 modelViewNormal = vec3(u_MVMatrix * vec4(a_Normal, 0.0));
-	// Will be used for attenuation.
-	float distance = length(u_LightPos - modelViewVertex);
-	// Get a lighting direction vector from the light to the vertex.
-	vec3 lightVector = normalize(u_LightPos - modelViewVertex);
-	// Calculate the dot product of the light vector and vertex normal. If the normal and light vector are
-	// pointing in the same direction then it will get max illumination.
-	float diffuse = max(dot(modelViewNormal, lightVector), 0.1);
-	// Attenuate the light based on distance.
-	diffuse = diffuse * (1.0 / (1.0 + (0.02 * distance * distance)));
-	// Multiply the color by the illumination level. It will be interpolated across the triangle.
-	v_Color = u_Color * diffuse;
+	vertexNormal = vec3(u_MVMatrix * vec4(a_Normal, 0.0));
+
+	v_Color = u_Color * ambientColor
+			+ lightVertex(light0Position, light0DiffuseColor)
+			+ lightVertex(light1Position, light1DiffuseColor);
+	v_Color = clamp(v_Color, 0.0, 1.0);
+
 	// gl_Position is a special variable used to store the final position.
 	// Multiply the vertex by the matrix to get the final point in normalized screen coordinates.
 	gl_Position = u_MVPMatrix * a_Position;
