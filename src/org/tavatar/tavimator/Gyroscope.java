7ebc06f44cc5deb1e9ef2ce7c37e975df549819e
Matthew Fulmer <Tapple Gao>
2013-02-07 19:06:38 -0700

    fixed a bug that caused the gyroscope to not work on nexus 4. Made sensor fusion be enabled or disabled at compile time. disabled it by default, as I don't feel like properly handling bad magnetometer readings

--- src/org/tavatar/tavimator/Gyroscope.java
+++ src/org/tavatar/tavimator/Gyroscope.java
@@ -11,115 +11,129 @@ import android.util.Log;
 import android.widget.Toast;
 
 public class Gyroscope implements SensorEventListener {
-	
-	private static final String TAG = "Gyroscope";
-    public static final float EPSILON = 0.000000001f;
-    private static final float NS2S = 1.0f / 1000000000.0f;
-    private static final float MS2S = 1.0f / 1000.0f;
 
-    private SensorManager sensorManager;
+	private static final String TAG = "Gyroscope";
+	public static final float EPSILON = 0.000000001f;
+	private static final float NS2S = 1.0f / 1000000000.0f;
+	private static final float MS2S = 1.0f / 1000.0f;
+
+	private static final boolean USE_SENSOR_FUSION = false;
+
+	private SensorManager sensorManager;
 	private Sensor accelerometer;
 	private Sensor magnetometer;
 	private Sensor gyrometer;
-	
+
 	private float[] gravity  = null;
 	private float[] magnetic = null;
 	private float[] angular  = new float[4];
-	
+
 	private int stabilityCountDown = 0;
-	
-    // rotation matrix from gyro data
-//    private float[] prevGyroMatrix = new float[16];
-//    private float[] gyroMatrix = new float[16];
- 
-    // orientation angles from gyro matrix
-	private float timestamp;
+
+	// rotation matrix from gyro data
+	//    private float[] prevGyroMatrix = new float[16];
+	//    private float[] gyroMatrix = new float[16];
+
+	// orientation angles from gyro matrix
+	private long timestamp;
 
 	private float[] accelMagOrientation = new float[16];
-    private float[] gyroOrientation = null;
-    private float[] inverseGyroOrientation = new float[16];
-    
+	private float[] gyroOrientation = null;
+	private float[] inverseGyroOrientation = new float[16];
+
 	private float[] deviceOrientation = null;
 	private float[] orientationOffset = null;
 	private float[] orientation = new float [16];
 	private float[] inverseOrientation = new float[16];
-	
+
 	/**
 	 * True if receiving sensor data
 	 */
 	private boolean sensing = false;
-	
+
 	/**
 	 * True if camera is following the device orientation
 	 */
 	private boolean tracking = false;
 	private boolean hasGyroscope = false;
-	
+	private boolean useAccelerometer = true;
+
 	public Gyroscope(Context context) {
 		Matrix.setIdentityM(orientation, 0);
 		Matrix.setIdentityM(inverseOrientation, 0);
-		
+
 		sensorManager = (SensorManager) context.getSystemService(Context.SENSOR_SERVICE);
 		accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
 		magnetometer  = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
-        gyrometer     = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+		gyrometer     = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
 
-        hasGyroscope = sensorManager.getSensorList(Sensor.TYPE_GYROSCOPE).size() > 0;
-        
-        if (!hasGyroscope) {
-        	Toast.makeText(context, R.string.no_gyroscope, Toast.LENGTH_LONG).show();
-        }
+		hasGyroscope = sensorManager.getSensorList(Sensor.TYPE_GYROSCOPE).size() > 0;
+		useAccelerometer = USE_SENSOR_FUSION || !hasGyroscope;
+
+		if (!hasGyroscope) {
+			Toast.makeText(context, R.string.no_gyroscope, Toast.LENGTH_LONG).show();
+		}
 	}
-	
-	protected void onResume() {
-        basicSetSensing(sensing);
-    }
 
-    protected void onPause() {
-        basicSetSensing(false);
-    }
-    
-    public void setSensing(boolean sensing) {
-    	if (this.sensing == sensing) return;
-    	this.sensing = sensing;
-    	basicSetSensing(sensing);
-    }
-    
-    private void basicSetSensing(boolean sensing) {
-    	if (sensing) {
-    		stabilityCountDown = 5;
-            sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_GAME);
-            sensorManager.registerListener(this, magnetometer, SensorManager.SENSOR_DELAY_GAME);
-            if (hasGyroscope) {
-            	sensorManager.registerListener(this, gyrometer, SensorManager.SENSOR_DELAY_GAME);
-            }
-    	} else {
-    		sensorManager.unregisterListener(this);
-    	}
+	protected void onResume() {
+		basicSetSensing(sensing);
+	}
+
+	protected void onPause() {
+		basicSetSensing(false);
+	}
+
+	public void setSensing(boolean sensing) {
+		if (this.sensing == sensing) return;
+		this.sensing = sensing;
+		basicSetSensing(sensing);
+	}
+
+	private void basicSetSensing(boolean sensing) {
+		if (sensing) {
+			if (useAccelerometer) {
+				stabilityCountDown = 5;
+				sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_GAME);
+				sensorManager.registerListener(this, magnetometer, SensorManager.SENSOR_DELAY_GAME);
+			}
+			if (hasGyroscope) {
+				sensorManager.registerListener(this, gyrometer, SensorManager.SENSOR_DELAY_GAME);
+			}
+		} else {
+			sensorManager.unregisterListener(this);
+		}
 		gravity = null;
 		magnetic = null;
-		gyroOrientation = null;
 		deviceOrientation = null;
 		orientationOffset = null;
-    }
 
-    public boolean getSensing() {
-    	return sensing;
-    }
+		if (useAccelerometer) {
+			gyroOrientation = null;
+		} else {
+			Log.d(TAG, "resetting gyro");
+			gyroOrientation = new float[16];
+			Matrix.setIdentityM(gyroOrientation, 0);
+		}
 
-    public void setTracking(boolean tracking) {
-    	if (this.tracking == tracking) return;
-    	this.tracking = tracking;
+	}
 
-    	// calculating the offset is done in updateOrientation. setting it to null triggers the calculation
-   		orientationOffset = null;
-    }
+	public boolean getSensing() {
+		return sensing;
+	}
 
-    public boolean getTracking() {
-    	return tracking;
-    }
+	public void setTracking(boolean tracking) {
+		if (this.tracking == tracking) return;
+		this.tracking = tracking;
 
- 
+		// calculating the offset is done in updateOrientation. setting it to null triggers the calculation
+		orientationOffset = null;
+	}
+
+	public boolean getTracking() {
+		return tracking;
+	}
+
+
 	public void onSensorChanged(SensorEvent evt) {
 		final float smoothing = 1.0f / 4.0f;
 		if (evt.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
@@ -151,7 +165,7 @@ public class Gyroscope implements SensorEventListener {
 				magnetic[2] = magnetic[2] * (1.0f - smoothing) + evt.values[2] * smoothing;
 			}
 		} else if (evt.sensor.getType() == Sensor.TYPE_GYROSCOPE) {
-	        gyroFunction(evt);
+			gyroFunction(evt);
 		}
 
 	}
@@ -160,17 +174,19 @@ public class Gyroscope implements SensorEventListener {
 	// It writes the gyroscope based orientation into gyroOrientation.
 	public void gyroFunction(SensorEvent event) {
 		// don't start until first accelerometer/magnetometer orientation has been acquired
-		if (gyroOrientation == null)
-			return;
-		
-//		if (true) return;
+		if (gyroOrientation == null) return;
 
 		angular[0] = (float)(event.values[0] * 180 / Math.PI);
 		angular[1] = (float)(event.values[1] * 180 / Math.PI);
 		angular[2] = (float)(event.values[2] * 180 / Math.PI);
 		angular[3] = 1.0f;
-		
+
 		final float dT = (event.timestamp - timestamp) * NS2S;
+
+		//		Log.d(TAG, "timestamp: " + event.timestamp +
+		//				"; dt: " + dT + " (" + (event.timestamp - timestamp) +
+		//				"); gyroEvent: " + TwoFingerTrackball.arrayToString(angular));
+
 		if(0.0f < dT && dT < 0.5f) {
 			float angularSpeed = Matrix.length(angular[0], angular[1], angular[2]);
 			if(angularSpeed > EPSILON) {
@@ -184,15 +200,15 @@ public class Gyroscope implements SensorEventListener {
 		// measurement done, save current time for next interval
 		timestamp = event.timestamp;
 	}
-    
+
 	public float[] getOrientation() {
 		return orientation;
 	}
-	
+
 	public float[] getInverseOrientation() {
 		return inverseOrientation;
 	}
-	
+
 	public float[] getAngularVelocity() {
 		float dT = SystemClock.uptimeMillis() * MS2S - timestamp * NS2S;
 		// if last gyro event was over half a second ago, device is not moving
@@ -204,7 +220,7 @@ public class Gyroscope implements SensorEventListener {
 		}
 		return angular;
 	}
-	
+
 	public void fuseSensorData() {
 		final float gyroCoeff = 0.99f;
 		final float accelCoeff = 1.0f - gyroCoeff;
@@ -216,7 +232,7 @@ public class Gyroscope implements SensorEventListener {
 		gyroAngles = Math3D.toEulerAngles(gyroAngles, gyroOrientation, BVHOrderType.BVH_XYZ);
 		accelAngles = Math3D.toEulerAngles(accelAngles, accelMagOrientation, BVHOrderType.BVH_XYZ);
 
-//*
+		//*
 		if ( gyroAngles.x < -90 && accelAngles.x > 0.0f)  gyroAngles.x += 360;
 		if (accelAngles.x < -90 &&  gyroAngles.x > 0.0f) accelAngles.x += 360;
 		if ( gyroAngles.y < -90 && accelAngles.y > 0.0f)  gyroAngles.y += 360;
@@ -233,17 +249,17 @@ public class Gyroscope implements SensorEventListener {
 		if (fusedAngles.z > 180) fusedAngles.z -= 360;
 
 
-//*/        fusedAngles = gyroAngles;
+		//*/        fusedAngles = gyroAngles;
 
 		Matrix.setIdentityM(gyroOrientation, 0);
 		Matrix.rotateM(gyroOrientation, 0, fusedAngles.x, 1, 0, 0);
 		Matrix.rotateM(gyroOrientation, 0, fusedAngles.y, 0, 1, 0);
 		Matrix.rotateM(gyroOrientation, 0, fusedAngles.z, 0, 0, 1);
-        
-//        System.arraycopy(deviceOrientation, 0, accelMagOrientation, 0, 16);
-//        Matrix.transposeM(deviceOrientation, 0, accelMagOrientation, 0);
+
+		//        System.arraycopy(deviceOrientation, 0, accelMagOrientation, 0, 16);
+		//        Matrix.transposeM(deviceOrientation, 0, accelMagOrientation, 0);
 	}
-	
+
 	public void updateOrientation() {
 		if (sensing && gravity != null && magnetic != null) {
 			SensorManager.getRotationMatrix(accelMagOrientation, null, gravity, magnetic);
@@ -252,13 +268,15 @@ public class Gyroscope implements SensorEventListener {
 				gyroOrientation = new float[16];
 				System.arraycopy(accelMagOrientation, 0, gyroOrientation, 0, 16);				
 			}
-			
+		}
+
+		if (gyroOrientation != null) {
 			if (deviceOrientation == null) {
 				deviceOrientation = new float[16];
 			}
-			
+
 			if (hasGyroscope) {
-				fuseSensorData();
+				if (USE_SENSOR_FUSION) fuseSensorData();
 				System.arraycopy(gyroOrientation, 0, deviceOrientation, 0, 16);
 			} else {
 				System.arraycopy(accelMagOrientation, 0, deviceOrientation, 0, 16);
@@ -281,7 +299,7 @@ public class Gyroscope implements SensorEventListener {
 	@Override
 	public void onAccuracyChanged(Sensor sensor, int accuracy) {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
+
 }
